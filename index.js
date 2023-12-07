3adc4878e91063fd2a0d7402d624ca676939e4b9
Obi1knube <123329625+Obi1knube@users.noreply.github.com>
2023-12-07 12:10:33 +0000

    program is executing but not displaying

--- index.js
+++ index.js
@@ -1,75 +1,52 @@
-
-
-// function promptUser():
-//   - prompt for logo text
-//   - prompt for text color
-//   - prompt for shape type
-//   - prompt for shape color
-//   - return user input
-
-// function main():
-//   - userInputs = promptUser()
-//   - logo = new Logo(userInputs)
-//   - logo.render()
-//   - saveSVGFile(logo)
-//   - printMessage("Generated logo.svg")
-
-// function saveSVGFile(logo):
-//   - create file "logo.svg"
-//   - write logo.render() to file
-
-// function printMessage(message):
-//   - print message to command line
-
-// main()
-
-
 const inquirer = require("inquirer");
 const { Circle, Square, Triangle, Logo } = require("./lib/shapes");
 
 function promptUser() {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "text",
-        message: "Enter logo text (up to 3 characters): ",
-      },
-      {
-        type: "input",
-        name: "textColor",
-        message: "Enter text color: ",
-      },
-      {
-        type: "list",
-        name: "shape",
-        message: "Select a shape ",
-        choices: ["circle", "square", "triangle"],
-      },
-      {
-        type: "input",
-        name: "shapeColor",
-        message: "Enter shape color: ",
-      },
-    ])
-    .then((answers) => {
-      // Use user feedback for... whatever!!
-      console.log(answers);
-      let shape = answers.shape;
-      switch (shape) {
-        case "circle":
-          return new Circle();
-        case "triangle":
-          return new Triangle();
-        case "square":
-          return new Square();
-        default:
-          console.log("Invalid shape type");
-      }
-      shape.setShapeColor(answers.shapeColor)
-      const logo = new Logo()
-      logo.setText(answers.textColor, answers.text)
-      logo.render()
-    });
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "text",
+        message: "Enter logo text (up to 3 characters): ",
+      },
+      {
+        type: "input",
+        name: "textColor",
+        message: "Enter text color: ",
+      },
+      {
+        type: "list",
+        name: "shape",
+        message: "Select a shape ",
+        choices: ["circle", "square", "triangle"],
+      },
+      {
+        type: "input",
+        name: "shapeColor",
+        message: "Enter shape color: ",
+      },
+    ])
+    .then((answers) => {
+      let shape;
+      switch (answers.shape) {
+        case "circle":
+          shape = new Circle();
+          break;
+        case "triangle":
+          shape = new Triangle();
+          break;
+        case "square":
+          shape = new Square();
+          break;
+        default:
+          console.log("Invalid shape type");
+      }
+      shape.setShapeColor(answers.shapeColor);
+      const logo = new Logo(shape, answers.text);
+      logo.setText(answers.textColor, answers.text);
+      const logoSvg = logo.render();
+      console.log(logoSvg);
+    });
 }
+
 promptUser();
