73659ccdcbde044eb02e68eb7cd962597ca99012
Obi1knube <123329625+Obi1knube@users.noreply.github.com>
2023-12-08 02:16:12 +0000

    shape.svg generated

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const inquirer = require("inquirer");
-const fs = require("fs");
 const { Circle, Square, Triangle, Logo } = require("./lib/shapes");
-const svg2png = require("svg2png");
 
 function promptUser() {
   inquirer
@@ -48,44 +46,7 @@ function promptUser() {
       logo.setText(answers.textColor, answers.text);
       const logoSvg = logo.render();
       console.log(logoSvg);
-
-      fs.writeFile("./lib/shape.svg", logoSvg, (err) => {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log("SVG file created successfully!");
-          convertSvgToPng("./lib/shape.svg", "./lib/shape.png").then(
-            (outputFilePath) => {
-              console.log(outputFilePath);
-            }
-          );
-        }
-      });
-    })
-    .catch((error) => {
-      if (error.isTtyError) {
-        // Prompt couldn't be rendered in the current environment
-      } else {
-        // Something else went wrong
-      }
     });
-}
-
-function convertSvgToPng(inputFilePath, outputFilePath) {
-  return new Promise((resolve, reject) => {
-    const input = fs.createReadStream(inputFilePath);
-    const output = fs.createWriteStream(outputFilePath);
-
-    input.pipe(svg2png()).pipe(output);
-
-    output.on("finish", () => {
-      resolve(outputFilePath);
-    });
-
-    output.on("error", (err) => {
-      reject(err);
-    });
-  });
 }
 
 promptUser();
