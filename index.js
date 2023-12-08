ac8dcea9de46fcfa3d456da653c27cfa2bd915af
Obi1knube <123329625+Obi1knube@users.noreply.github.com>
2023-12-08 22:59:12 +0000

    convert-svg-to-png import is not working correctly

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
 const { Circle, Square, Triangle, Logo } = require("./lib/shapes");
+const { convertSvgToPng } = require('convert-svg-to-png');
 
-function promptUser() {
+// Function to generate the output file path
+const outputFilePath = () => {
+  const outputDirectory = "./lib/";
+  const outputFileName = "image.png";
+  return outputDirectory + outputFileName;
+};
+
+// Function to convert SVG to PNG
+const convertSvgToPngFile = async () => {
+  const inputFilePath = "./lib/shape.svg";
+ // const outputFilePath = outputFilePath();
+
+  // Your code for converting SVG to PNG goes here
+  console.log(outputFilePath);
+  (async () => {
+    const inputFilePath = "./lib/shape.svg";
+    const outputFilePath = await convertSvgToPng(inputFilePath);
+
+    console.log(outputFilePath);
+  })();
+};
+
+// Function to prompt the user for logo characteristics
+const promptUser = () => {
   inquirer
     .prompt([
       {
@@ -27,26 +52,73 @@ function promptUser() {
       },
     ])
     .then((answers) => {
-      let shape;
-      switch (answers.shape) {
-        case "circle":
-          shape = new Circle();
-          break;
-        case "triangle":
-          shape = new Triangle();
-          break;
-        case "square":
-          shape = new Square();
-          break;
-        default:
-          console.log("Invalid shape type");
-      }
+      const shape = createShape(answers.shape);
       shape.setShapeColor(answers.shapeColor);
+
       const logo = new Logo(shape, answers.text);
       logo.setText(answers.textColor, answers.text);
+
       const logoSvg = logo.render();
       console.log(logoSvg);
-    });
-}
 
+      writeSvgToFile(logoSvg);
+    });
+};
+
+// Function to create the appropriate shape object based on user input
+const createShape = (shapeType) => {
+  let shape;
+  switch (shapeType) {
+    case "circle":
+      shape = new Circle();
+      break;
+    case "triangle":
+      shape = new Triangle();
+      break;
+    case "square":
+      shape = new Square();
+      break;
+    default:
+      console.log("Invalid shape type");
+  }
+  return shape;
+};
+
+// Function to write the generated SVG logo to a file
+const writeSvgToFile = (logoSvg) => {
+  fs.writeFile("./lib/shape.svg", logoSvg, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("SVG file created successfully!");
+      convertSvgToPngFile();
+    }
+  });
+};
+
+// Call the promptUser function to start the application
 promptUser();
+
+/*In this refactored version, I've separated the concerns into different functions:
+
+
+
+outputFilePath: This function generates the output file path for the PNG file.
+
+
+
+convertSvgToPng: This function handles the conversion of the SVG file to a PNG file. You can add your code for converting SVG to PNG in this function.
+
+
+
+promptUser: This function prompts the user for logo characteristics using the inquirer package. It then creates the appropriate shape object based on the user's input, sets the shape color and text for the logo, renders the logo SVG, and writes it to a file.
+
+
+
+createShape: This function creates the appropriate shape object based on the user's input for the shape type.
+
+
+
+writeSvgToFile: This function writes the generated SVG logo to a file and calls the convertSvgToPng function.
+
+*/
